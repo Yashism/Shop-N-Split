ca10dd8f2a35663a9a56da37a28746e10e23eaf2
Yash Waghmare <69153292+Yashism@users.noreply.github.com>
2023-04-19 01:26:20 -0500

    Add files via upload

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/akshaj02/shopnsplit/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.akshaj02.shopnsplit.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.akshaj02.shopnsplit.test";
+  public static final String BUILD_TYPE = "debug";
+}
